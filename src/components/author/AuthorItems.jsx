aa4b25dfc2902d2b5bcf9ca1e71a9f8b37bfe7b3
sebastian <manijustworkhere@gmail.com>
2025-11-15 14:35:21 -0500

    in this branch i fixed the author page and made it completely dynamic to each author id and made the follow button at least look like its functional

--- src/components/author/AuthorItems.jsx
+++ src/components/author/AuthorItems.jsx
@@ -1,65 +1,143 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
 
-const AuthorItems = () => {
-  return (
-    <div className="de_tab_content">
-      <div className="tab-1">
-        <div className="row">
-          {new Array(8).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft__item">
-                <div className="author_list_pp">
-                  <Link to="">
-                    <img className="lazy" src={AuthorImage} alt="" />
-                    <i className="fa fa-check"></i>
-                  </Link>
+const skeletonStyles = {
+  item: {
+    borderRadius: "12px",
+    background: "#e2e5e7",
+    padding: "24px 16px 20px 16px",
+    minHeight: "320px",
+    marginBottom: "24px",
+  },
+  avatar: {
+    borderRadius: "50%",
+    width: "64px",
+    height: "64px",
+    background: "#cfd2d4",
+    margin: "0 auto 18px auto",
+    display: "block",
+  },
+  image: {
+    borderRadius: "12px",
+    width: "100%",
+    height: "170px",
+    background: "#d8dadb",
+    display: "block",
+    marginBottom: "15px",
+  },
+  title: {
+    background: "#cfd2d4",
+    height: "18px",
+    width: "70%",
+    borderRadius: "6px",
+    margin: "12px auto 10px auto",
+  },
+  price: {
+    background: "#cfd2d4",
+    height: "15px",
+    width: "30%",
+    borderRadius: "6px",
+    margin: "0 auto 10px auto",
+  },
+  likes: {
+    background: "#cfd2d4",
+    height: "14px",
+    width: "18%",
+    borderRadius: "4px",
+    margin: "0 auto",
+  },
+};
+
+const AuthorItems = ({ nfts, loading, authorImage }) => (
+  <div className="de_tab_content">
+    <div className="tab-1">
+      <div className="row">
+        {loading
+          ? [...Array(8)].map((_, index) => (
+              <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+                <div style={skeletonStyles.item}>
+                  <div style={skeletonStyles.avatar}></div>
+                  <div style={skeletonStyles.image}></div>
+                  <div style={skeletonStyles.title}></div>
+                  <div style={skeletonStyles.price}></div>
+                  <div style={skeletonStyles.likes}></div>
                 </div>
-                <div className="nft__item_wrap">
-                  <div className="nft__item_extra">
-                    <div className="nft__item_buttons">
-                      <button>Buy Now</button>
-                      <div className="nft__item_share">
-                        <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-facebook fa-lg"></i>
-                        </a>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-twitter fa-lg"></i>
-                        </a>
-                        <a href="">
-                          <i className="fa fa-envelope fa-lg"></i>
-                        </a>
+              </div>
+            ))
+          : nfts?.map((nft, index) => (
+              <div
+                className="col-lg-3 col-md-6 col-sm-6 col-xs-12"
+                key={nft.nftId || index}
+              >
+                <div className="nft__item">
+                  <div className="author_list_pp">
+                    <Link to={`/author/${nft.authorId}`}>
+                      <img
+                        className="nft__img--author"
+                        src={nft.authorImage || authorImage}
+                        alt={nft.authorName || "NFT Author"}
+                        style={{
+                          width: "60px",
+                          height: "60px",
+                          borderRadius: "50%",
+                          objectFit: "cover",
+                        }}
+                        onError={(e) => {
+                          e.target.onerror = null;
+                        }}
+                      />
+                      <i className="fa fa-check nft__icon--verified"></i>
+                    </Link>
+                  </div>
+                  <div className="nft__item_wrap">
+                    <div className="nft__item_extra">
+                      <div className="nft__item_buttons">
+                        <button className="nft__button--buy">Buy Now</button>
+                        <div className="nft__item_share">
+                          <h4>Share</h4>
+                          <a href="#" target="_blank" rel="noreferrer">
+                            <i className="fa fa-facebook fa-lg"></i>
+                          </a>
+                          <a href="#" target="_blank" rel="noreferrer">
+                            <i className="fa fa-twitter fa-lg"></i>
+                          </a>
+                          <a href="#">
+                            <i className="fa fa-envelope fa-lg"></i>
+                          </a>
+                        </div>
                       </div>
                     </div>
+                    <Link to={`/item-details/${nft.nftId}`}>
+                      <img
+                        src={nft.nftImage}
+                        className="nft__item_preview"
+                        alt={nft.title}
+                        loading="lazy"
+                        style={{
+                          borderRadius: "12px",
+                          width: "100%",
+                          height: "170px",
+                          objectFit: "cover",
+                        }}
+                      />
+                    </Link>
                   </div>
-                  <Link to="/item-details">
-                    <img
-                      src={nftImage}
-                      className="lazy nft__item_preview"
-                      alt=""
-                    />
-                  </Link>
-                </div>
-                <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <div className="nft__item_price">2.52 ETH</div>
-                  <div className="nft__item_like">
-                    <i className="fa fa-heart"></i>
-                    <span>97</span>
+                  <div className="nft__item_info">
+                    <Link to={`/item-details/${nft.nftId}`}>
+                      <h4 className="nft__title">{nft.title}</h4>
+                    </Link>
+                    <div className="nft__item_price">{nft.price} ETH</div>
+                    <div className="nft__item_like">
+                      <i className="fa fa-heart"></i>
+                      <span>{nft.likes}</span>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default AuthorItems;
--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,23 +1,55 @@
-import React from "react";
-import AuthorBanner from "../images/author_banner.jpg";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import AuthorItems from "../components/author/AuthorItems";
-import { Link } from "react-router-dom";
-import AuthorImage from "../images/author_thumbnail.jpg";
+import AuthorBanner from "../images/author_banner.jpg";
+
+const skeletonBox = {
+  background: "#e2e5e7",
+  borderRadius: "10px",
+  width: "100%",
+  margin: "8px auto",
+  minHeight: "22px",
+};
 
 const Author = () => {
+  const { authorId } = useParams();
+  const [author, setAuthor] = useState(null);
+  const [followers, setFollowers] = useState(0);
+  const [hasFollowed, setHasFollowed] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  const effectiveAuthorId = authorId || "73855012";
+
+  useEffect(() => {
+    setLoading(true);
+    setHasFollowed(false);
+    fetch(
+      `https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${effectiveAuthorId}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setAuthor(data);
+        setFollowers(data.followers);
+        setLoading(false);
+      });
+  }, [effectiveAuthorId]);
+
+  const handleFollowToggle = () => {
+    setFollowers((prev) => prev + (hasFollowed ? -1 : 1));
+    setHasFollowed((prev) => !prev);
+  };
+
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
         <div id="top"></div>
-
         <section
           id="profile_banner"
           aria-label="section"
           className="text-light"
-          data-bgimage="url(images/author_banner.jpg) top"
+          data-bgimage={`url(${AuthorBanner}) top`}
           style={{ background: `url(${AuthorBanner}) top` }}
-        ></section>
-
+        />
         <section aria-label="section">
           <div className="container">
             <div className="row">
@@ -25,37 +57,123 @@ const Author = () => {
                 <div className="d_profile de-flex">
                   <div className="de-flex-col">
                     <div className="profile_avatar">
-                      <img src={AuthorImage} alt="" />
-
-                      <i className="fa fa-check"></i>
+                      {loading ? (
+                        <div>
+                          <div
+                            style={{
+                              ...skeletonBox,
+                              width: "90px",
+                              height: "90px",
+                              borderRadius: "50%",
+                              margin: "0 auto 14px auto",
+                            }}
+                          />
+                          <div
+                            style={{
+                              ...skeletonBox,
+                              width: "80%",
+                              minHeight: "22px",
+                            }}
+                          />
+                          <div
+                            style={{
+                              ...skeletonBox,
+                              width: "60%",
+                              minHeight: "18px",
+                            }}
+                          />
+                        </div>
+                      ) : (
+                        <img
+                          src={author.authorImage}
+                          alt={author.authorName}
+                          className="profile_avatar__img"
+                          style={{
+                            width: "90px",
+                            height: "90px",
+                            borderRadius: "50%",
+                            objectFit: "cover",
+                          }}
+                          onError={(e) => {
+                            e.target.onerror = null;
+                          }}
+                        />
+                      )}
+                      <i className="fa fa-check profile_avatar__icon" />
                       <div className="profile_name">
                         <h4>
-                          Monica Lucas
-                          <span className="profile_username">@monicaaaa</span>
-                          <span id="wallet" className="profile_wallet">
-                            UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
-                          </span>
-                          <button id="btn_copy" title="Copy Text">
-                            Copy
-                          </button>
+                          {loading ? (
+                            <>
+                              <div
+                                style={{
+                                  ...skeletonBox,
+                                  width: "48%",
+                                  minHeight: "18px",
+                                }}
+                              />
+                            </>
+                          ) : (
+                            <>
+                              {author?.authorName}
+                              <span className="profile_username">
+                                @{author?.tag}
+                              </span>
+                              <span id="wallet" className="profile_wallet">
+                                {author?.address}
+                              </span>
+                              <button id="btn_copy" title="Copy Text">
+                                Copy
+                              </button>
+                            </>
+                          )}
                         </h4>
                       </div>
                     </div>
                   </div>
                   <div className="profile_follow de-flex">
                     <div className="de-flex-col">
-                      <div className="profile_follower">573 followers</div>
-                      <Link to="#" className="btn-main">
-                        Follow
-                      </Link>
+                      {loading ? (
+                        <>
+                          <div
+                            style={{
+                              ...skeletonBox,
+                              width: "70px",
+                              minHeight: "22px",
+                            }}
+                          />
+                          <div
+                            style={{
+                              ...skeletonBox,
+                              width: "110px",
+                              minHeight: "36px",
+                              borderRadius: "8px",
+                            }}
+                          />
+                        </>
+                      ) : (
+                        <>
+                          <div className="profile_follower">
+                            {followers} followers
+                          </div>
+                          <button
+                            className="btn-main nav__link--primary"
+                            onClick={handleFollowToggle}
+                          >
+                            {hasFollowed ? "Unfollow" : "Follow"}
+                          </button>
+                        </>
+                      )}
                     </div>
                   </div>
                 </div>
               </div>
-
               <div className="col-md-12">
                 <div className="de_tab tab_simple">
-                  <AuthorItems />
+                  <AuthorItems
+                    nfts={author?.nftCollection}
+                    loading={loading}
+                    authorImage={author?.authorImage}
+                  />
                 </div>
               </div>
             </div>
