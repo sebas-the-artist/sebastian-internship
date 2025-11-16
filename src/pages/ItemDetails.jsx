96d55bafcc163dd9eee74e2d35a697d0a327d47e
sebastian <manijustworkhere@gmail.com>
2025-11-16 07:21:51 -0500

    touched up a little bit

--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -39,7 +39,6 @@ const ItemDetails = () => {
         <section aria-label="section" className="mt90 sm-mt-0">
           <div className="container">
             <div className="row">
-              {/* Left side NFT image */}
               <div className="col-md-6 text-center">
                 {loading ? (
                   <div
@@ -58,7 +57,6 @@ const ItemDetails = () => {
                   />
                 )}
               </div>
-              {/* Right side NFT info */}
               <div className="col-md-6">
                 <div className="item_info">
                   {loading ? (
@@ -87,7 +85,7 @@ const ItemDetails = () => {
                             ...skeletonBox,
                             width: "80px",
                             display: "inline-block",
-                            marginLeft: "12px",
+                            marginLeft: "11px",
                           }}
                         />
                       </>
@@ -112,9 +110,7 @@ const ItemDetails = () => {
                     <p>{item?.description}</p>
                   )}
 
-                  {/* Owner / Creator vertically stacked */}
                   <div style={{ marginTop: "28px" }}>
-                    {/* Owner */}
                     <div style={{ marginBottom: "16px" }}>
                       <h6 style={{ fontWeight: "600", marginBottom: "6px" }}>
                         Owner
@@ -166,7 +162,6 @@ const ItemDetails = () => {
                       )}
                     </div>
 
-                    {/* Creator */}
                     <div style={{ marginBottom: "24px" }}>
                       <h6 style={{ fontWeight: "600", marginBottom: "6px" }}>
                         Creator
@@ -218,7 +213,6 @@ const ItemDetails = () => {
                       )}
                     </div>
 
-                    {/* Price */}
                     <div>
                       <h6 style={{ fontWeight: "600", marginBottom: "6px" }}>
                         Price
@@ -251,227 +245,6 @@ const ItemDetails = () => {
                       )}
                     </div>
                   </div>
-                  {/* end owner/creator/price section */}
-                </div>
-              </div>
-              {/* end right */}
-            </div>
-          </div>
-        </section>
-      </div>
-    </div>
-  );
-};
-
-export default ItemDetails;
-
-/* import React, { useEffect, useState } from "react";
-import { useParams, Link } from "react-router-dom";
-import EthImage from "../images/ethereum.svg";
-
-const skeletonBox = {
-  background: "#e2e5e7",
-  borderRadius: "10px",
-  width: "100%",
-  margin: "8px 0",
-  minHeight: "20px",
-};
-
-const ItemDetails = () => {
-  const { nftId } = useParams();
-  const [item, setItem] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    if (!nftId) return;
-    setLoading(true);
-    fetch(
-      `https://us-central1-nft-cloud-functions.cloudfunctions.net/itemDetails?nftId=${nftId}`
-    )
-      .then((res) => {
-        if (!res.ok) throw new Error("Network error");
-        return res.json();
-      })
-      .then((data) => {
-        setItem(data);
-        setLoading(false);
-      })
-      .catch(() => setLoading(false));
-  }, [nftId]);
-
-  return (
-    <div id="wrapper">
-      <div className="no-bottom no-top" id="content">
-        <div id="top" />
-        <section aria-label="section" className="mt90 sm-mt-0">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-6 text-center">
-                {loading ? (
-                  <div
-                    style={{
-                      ...skeletonBox,
-                      height: "360px",
-                      borderRadius: "16px",
-                      margin: "0 auto",
-                    }}
-                  />
-                ) : (
-                  <img
-                    src={item?.nftImage}
-                    alt={item?.title}
-                    className="img-fluid img-rounded mb-sm-30 nft-image"
-                  />
-                )}
-              </div>
-              <div className="col-md-6">
-                <div className="item_info">
-                  {loading ? (
-                    <div
-                      style={{
-                        ...skeletonBox,
-                        width: "70%",
-                        minHeight: "28px",
-                      }}
-                    />
-                  ) : (
-                    <h2>{item?.title}</h2>
-                  )}
-                  <div className="item_info_counts">
-                    {loading ? (
-                      <>
-                        <div
-                          style={{
-                            ...skeletonBox,
-                            width: "80px",
-                            display: "inline-block",
-                          }}
-                        />
-                        <div
-                          style={{
-                            ...skeletonBox,
-                            width: "80px",
-                            display: "inline-block",
-                            marginLeft: "12px",
-                          }}
-                        />
-                      </>
-                    ) : (
-                      <>
-                        <div className="item_info_views">
-                          <i className="fa fa-eye" /> {item?.views}
-                        </div>
-                        <div className="item_info_like">
-                          <i className="fa fa-heart" /> {item?.likes}
-                        </div>
-                      </>
-                    )}
-                  </div>
-                  {loading ? (
-                    <>
-                      <div style={skeletonBox} />
-                      <div style={skeletonBox} />
-                      <div style={{ ...skeletonBox, width: "80%" }} />
-                    </>
-                  ) : (
-                    <p>{item?.description}</p>
-                  )}
-                  <div className="d-flex flex-row">
-                    <div className="mr40">
-                      <h6>Owner</h6>
-                      <div className="item_author">
-                        {loading ? (
-                          <div
-                            style={{
-                              ...skeletonBox,
-                              width: "50px",
-                              height: "50px",
-                              borderRadius: "50%",
-                            }}
-                          />
-                        ) : (
-                          <Link to={`/author/${item?.ownerId}`}>
-                            <img
-                              src={item?.ownerImage}
-                              alt={item?.ownerName}
-                              className="lazy"
-                              style={{
-                                width: "50px",
-                                height: "50px",
-                                borderRadius: "50%",
-                                objectFit: "cover",
-                              }}
-                            />
-                            <i className="fa fa-check" />
-                          </Link>
-                        )}
-                        {!loading && (
-                          <div className="author_list_info">
-                            <Link to={`/author/${item?.ownerId}`}>
-                              {item?.ownerName}
-                            </Link>
-                          </div>
-                        )}
-                      </div>
-                    </div>
-                    <div className="mr40">
-                      <h6>Creator</h6>
-                      <div className="item_author">
-                        {loading ? (
-                          <div
-                            style={{
-                              ...skeletonBox,
-                              width: "50px",
-                              height: "50px",
-                              borderRadius: "50%",
-                            }}
-                          />
-                        ) : (
-                          <Link to={`/author/${item?.creatorId}`}>
-                            <img
-                              src={item?.creatorImage}
-                              alt={item?.creatorName}
-                              className="lazy"
-                              style={{
-                                width: "50px",
-                                height: "50px",
-                                borderRadius: "50%",
-                                objectFit: "cover",
-                              }}
-                            />
-                            <i className="fa fa-check" />
-                          </Link>
-                        )}
-                        {!loading && (
-                          <div className="author_list_info">
-                            <Link to={`/author/${item?.creatorId}`}>
-                              {item?.creatorName}
-                            </Link>
-                          </div>
-                        )}
-                      </div>
-                    </div>
-                  </div>
-                  <div className="de_tab tab_simple">
-                    <div className="de_tab_content">
-                      <div className="spacer-40" />
-                      <h6>Price</h6>
-                      {loading ? (
-                        <div
-                          style={{
-                            ...skeletonBox,
-                            width: "120px",
-                            minHeight: "32px",
-                          }}
-                        />
-                      ) : (
-                        <div className="nft-item-price">
-                          <img src={EthImage} alt="ETH" />
-                          <span>{item?.price}</span>
-                        </div>
-                      )}
-                    </div>
-                  </div>
                 </div>
               </div>
             </div>
@@ -483,4 +256,3 @@ const ItemDetails = () => {
 };
 
 export default ItemDetails;
- */
