66d3bbd6ee116347f0b81782723d96506a793310
sebastian <manijustworkhere@gmail.com>
2025-11-17 04:42:48 -0500

    just finished the animation for the landing features, i made them fade up in an arrow formation kind of a nice touch in my opinion

--- src/components/home/LandingIntro.jsx
+++ src/components/home/LandingIntro.jsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const LandingIntro = () => {
+  useEffect(() => {
+    AOS.init({ duration: 1000, once: false });
+
+    const handleLoad = () => {
+      AOS.refresh();
+    };
+
+    window.addEventListener("load", handleLoad);
+
+    return () => {
+      window.removeEventListener("load", handleLoad);
+    };
+  }, []);
+
   return (
     <section id="section-intro" className="no-top no-bottom">
       <div className="container">
         <div className="row">
           <div className="col-lg-4 col-md-6 mb-sm-30">
-            <div className="feature-box f-boxed style-3">
+            <div
+              className="feature-box f-boxed style-3"
+              data-aos="fade-up-right"
+              data-aos-delay="300"
+            >
               <i className="bg-color-2 i-boxed icon_wallet"></i>
               <div className="text">
                 <h4 className="">Set up your wallet</h4>
@@ -19,7 +40,11 @@ const LandingIntro = () => {
             </div>
           </div>
           <div className="col-lg-4 col-md-6 mb-sm-30">
-            <div className="feature-box f-boxed style-3">
+            <div
+              className="feature-box f-boxed style-3"
+              data-aos="fade-up"
+              data-aos-delay="400"
+            >
               <i className="bg-color-2 i-boxed icon_cloud-upload_alt"></i>
               <div className="text">
                 <h4 className="">Add your NFT's</h4>
@@ -32,7 +57,11 @@ const LandingIntro = () => {
             </div>
           </div>
           <div className="col-lg-4 col-md-6 mb-sm-30">
-            <div className="feature-box f-boxed style-3">
+            <div
+              className="feature-box f-boxed style-3"
+              data-aos="fade-up-left"
+              data-aos-delay="400"
+            >
               <i className="bg-color-2 i-boxed icon_tags_alt"></i>
               <div className="text">
                 <h4 className="">Sell your NFT's</h4>
