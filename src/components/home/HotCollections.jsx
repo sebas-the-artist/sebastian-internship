9d02558ab8bb4ba1a5fbdba049e2a7735fa5dc3b
sebastian <manijustworkhere@gmail.com>
2025-11-24 15:51:52 -0500

    maannnn i thought i understood, but nope

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -95,7 +95,7 @@ const HotCollections = () => {
   }, []);
 
   const responsive = {
-    0: { items: 1 },
+    1: { items: 1 },
     600: { items: 2 },
     900: { items: 3 },
     1200: { items: 4 },
